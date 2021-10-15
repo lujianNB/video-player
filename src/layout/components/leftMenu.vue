bd3f3edb99f2967922196d13a750b894cff3f179
lujian <lujiannb@qq.com>
2021-10-15 18:22:07 +0800

    feat: hls视频流播放

---
+++ src/layout/components/leftMenu.vue
@@ -0,0 +1,168 @@
+<!--
+ * @Autor: 卢建
+ * @LastEditors: 卢建
+ * @Description: 菜单
+ * @Date: 2020-11-23 16:40:33
+ * @LastEditTime: 2021-09-07 15:53:21
+-->
+<template>
+  <div class="lj-left-menu">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-vertical-demo"
+      background-color="#242F4B"
+      text-color="#D2D5DE"
+      active-text-color="#468FFE"
+      ref="activeIndex"
+    >
+      <menu-item :menuData="menuData"></menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+import menuItem from "./menuItem";
+export default {
+  name: "leftMenu",
+
+  components: {
+    menuItem,
+  },
+
+  data() {
+    return {
+      activeIndex: null, //当前菜单展开
+      menuData: [
+        {
+          id: "0",
+          title: "首页",
+          router: "/home",
+          icon: "el-icon-s-home",
+          children: [],
+        },
+        {
+          id: "1",
+          title: "海康视频",
+          icon: "el-icon-place",
+          router: "/vedio",
+          children: [],
+        },
+        {
+          id: "2",
+          title: "设备管理",
+          icon: "el-icon-s-management",
+          router: "",
+          children: [
+            {
+              id: "2-0",
+              title: "设备管理",
+              icon: "",
+              router: "/home",
+              children: [],
+            },
+            {
+              id: "2-1",
+              title: "设备生命周期管理",
+              icon: "",
+              router: "/home",
+              children: [],
+            },
+          ],
+        },
+        {
+          id: "3",
+          title: "告警管理",
+          icon: "el-icon-sunrise",
+          router: "/home",
+          children: [
+            {
+              id: "3-0",
+              title: "巡检告警",
+              icon: "",
+              router: "/home",
+              children: [],
+            },
+          ],
+        },
+        {
+          id: "4",
+          title: "消息中心",
+          icon: "el-icon-chat-dot-square",
+          router: "/home",
+          children: [
+            {
+              id: "4-0",
+              title: "消息列表",
+              icon: "",
+              router: "/home",
+              children: [],
+            },
+          ],
+        },
+        {
+          id: "5",
+          title: "系统管理",
+          icon: "el-icon-setting",
+          router: "/home",
+          children: [
+            {
+              id: "5-0",
+              title: "用户管理",
+              icon: "",
+              router: "/home",
+              children: [
+                {
+                  id: "5-0-1",
+                  title: "权限管理",
+                  icon: "",
+                  router: "/home",
+                  children: [],
+                },
+              ],
+            },
+            {
+              id: "5-1",
+              title: "角色管理",
+              icon: "",
+              router: "/home",
+              children: [],
+            },
+            {
+              id: "5-2",
+              title: "权限管理",
+              icon: "",
+              router: "/home",
+              children: [],
+            },
+          ],
+        },
+      ],
+    };
+  },
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {},
+
+  watch: {
+    $route: {
+      handler(val) {
+        // 监听路由，确保刷新当前页面与菜单一致。
+        this.activeIndex = val.meta.id;
+      },
+      deep: true,
+      immediate: true,
+    },
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.lj-left-menu {
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+}
+</style>
---
+++ src/router/router.js
@@ -0,0 +1,46 @@
+/*
+ * @Autor: 卢建
+ * @LastEditors: 卢建
+ * @Description: 路由表
+ * @Date: 2020-11-19 14:01:23
+ * @LastEditTime: 2021-09-07 15:51:51
+ */
+import Home from '../views/Home.vue'
+import Layout from '@/layout'
+
+export const routes = [{
+        path: '/',
+        name: 'Layout',
+        component: Layout,
+        redirect: '/home',
+        meta: {
+            pathName: '基础布局'
+        },
+        children: [{
+                path: '/home',
+                name: 'Home',
+                component: Home,
+                meta: {
+                    pathName: '首页',
+                    id: '0'
+                }
+            },
+            {
+                path: '/vedio',
+                name: 'Vedio',
+                component: () => import( /* webpackChunkName: "vedio" */ '../views/vedio'),
+                meta: {
+                    pathName: '视频',
+                    id: '1'
+                }
+            },
+        ]
+    },
+    {
+        path: '*', // 必须放在配置最下面 - 错误路由配置
+        component: () => import( /* webpackChunkName: "error" */ '../views/error'),
+        meta: {
+            pathName: '错误页'
+        }
+    }
+]
